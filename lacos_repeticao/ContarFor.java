2a00cb87b533987331067d95fdda73b1165f2273
attiquetecnologia <rodrigoatique@gmail.com>
2025-08-12 08:09:30 -0300

    loop for python com outras linguagens e pypy

--- lacos_repeticao/ContarFor.java
+++ lacos_repeticao/ContarFor.java
@@ -1,9 +1,14 @@
-import java.lang.Math;
 public class ContarFor {
     public static void main(String[] args){
-
-        for (int i=0; i<=1_000_000; i++){
-            
-        }
+        long tempoInicial = System.currentTimeMillis();
+        
+        for (int i=0; i<=1_000_000; i++){}
+        
+        long tempoFinal = System.currentTimeMillis();
+        System.out.printf("%f ms%n", (tempoFinal - tempoInicial) / 1000d);
+        // >>> 0,002000 ms
+        // >>> 0,006000 ms
+        // >>> 0,006000 ms
+        // >>> 0,005000 ms
     }
 }
--- lacos_repeticao/contar_for.py
+++ lacos_repeticao/contar_for.py
@@ -6,19 +6,27 @@ for p in range(1_000_000):
     pass
 end = time.perf_counter()
 
-print(f"Tempo de execução - contar até 1mi {(end-start)} s")
-# >>> Tempo de execução - contar até 1mi 0.051368702000218036 s
-# >>> Tempo de execução - contar até 1mi 0.0739227670001128 s
-# >>> Tempo de execução - contar até 1mi 0.08455388199945446 s
-# >>> Tempo de execução - contar até 1mi 0.07860575299946504 s
+print(f"Tempo de execução - contar até 1mi {(end-start)* 1000:.6f} ms")
+# Tempo de execução - contar até 1mi 70.638609 ms
+# pypy: 
+# Tempo de execução - contar até 1mi 16.945604 ms
+# Tempo de execução - contar até 1mi 10.941131 ms
+# pyc - cpython:
+# Tempo de execução - contar até 1mi 69.843812 ms
+# Tempo de execução - contar até 1mi 90.051050 ms
+# Tempo de execução - contar até 1mi 95.942532 ms
 
 # Compressão de Listas
 start = time.perf_counter()
 contar = [p for p in range(1_000_000)]
 end = time.perf_counter()
 
-print(f"Tempo de execução - contar até 1mi {(end-start)} s")
-# >>> Tempo de execução - contar até 1mi 0.030854626999825996 s
-# >>> Tempo de execução - contar até 1mi 0.03853460400023323 s
-# >>> Tempo de execução - contar até 1mi 0.04224357200018858 s
-# >>> Tempo de execução - contar até 1mi 0.03570912500072154 s
+print(f"Tempo de execução - contar até 1mi {(end-start)* 1000:.6f} ms")
+# Tempo de execução - contar até 1mi 31.542276 ms
+# pypy: 
+# Tempo de execução - contar até 1mi 9.192279 ms
+# Tempo de execução - contar até 1mi 9.828184 ms
+# pyc - cpython:
+# Tempo de execução - contar até 1mi 33.817559 ms
+# Tempo de execução - contar até 1mi 39.015471 ms
+# Tempo de execução - contar até 1mi 49.092533 ms
