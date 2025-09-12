7afbaf338511453108ad795b300e8899d9a0fc0e
attiquetecnologia <rodrigoatique>
2025-09-12 07:21:29 -0300

    node express

---
+++ frameworks/react/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Olá, Mundo!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
